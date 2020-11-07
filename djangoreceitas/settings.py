b175afdc8b2c84ee29011392fda9c3e0990c063a
Bruno Cesar <bruunoocesar@gmail.com>
2020-11-07 12:26:36 -0300

    Configura banco de dados postgresql

--- djangoreceitas/settings.py
+++ djangoreceitas/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'djangoreceitas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_receitas',
+        'USER': 'dev',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
     }
 }
 
