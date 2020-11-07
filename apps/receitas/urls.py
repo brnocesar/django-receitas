e21e83f82013506b68e4906db1c5b089186c8626
Bruno Cesar <bruunoocesar@gmail.com>
2020-11-07 16:03:44 -0300

    Envia parametro pela URL e recupera objeto pela pk

--- apps/receitas/urls.py
+++ apps/receitas/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('receita', views.receita, name='receita')
+    path('<int:receita_id>', views.receita, name='receita')
 ]
--- apps/receitas/views.py
+++ apps/receitas/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Receita
 
 def index(request):
     return render(request, 'index.html', {'receitas': Receita.objects.all()})
 
-def receita(request):
-    return render(request, 'receita.html')
+def receita(request, receita_id):
+    return render(request, 'receita.html', {'receita': get_object_or_404(Receita, pk=receita_id)})
