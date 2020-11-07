7fa5a09286d9fef71764b1cf097926071b3bd6ab
Bruno Cesar <bruunoocesar@gmail.com>
2020-11-07 14:01:34 -0300

    Cria model Receita

--- apps/receitas/models.py
+++ apps/receitas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Receita(models.Model):
+    nome          = models.CharField(max_length=200)
+    ingredientes  = models.TextField()
+    modo_preparo  = models.TextField()
+    tempo_preparo = models.IntegerField()
+    rendimento    = models.CharField(max_length=100)
+    categoria     = models.CharField(max_length=100)
+    data_criacao  = models.DateTimeField(default=datetime.now, blank=True)
