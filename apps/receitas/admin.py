cb855daac4f4385ffb7567ad9c18889cc4588720
Bruno Cesar <bruunoocesar@gmail.com>
2020-11-07 21:06:32 -0300

    Customiza listagem de receitas no Django Admin

--- apps/receitas/admin.py
+++ apps/receitas/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Receita
 
-admin.site.register(Receita)
+class IndexReceita(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'categoria', 'tempo_preparo')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_filter = ('categoria',)
+    list_per_page = 10
+
+admin.site.register(Receita, IndexReceita)
