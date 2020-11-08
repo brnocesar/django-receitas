d502182088b58dbd1d348e1415b0497c51cfce17
Bruno Cesar <bruunoocesar@gmail.com>
2020-11-07 21:37:12 -0300

    Cria app de pessoas

---
+++ apps/pessoas/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class Pessoa(models.Model):
+    nome  = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
--- djangoreceitas/settings.py
+++ djangoreceitas/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.receitas',
+    'apps.pessoas',
 ]
 
 MIDDLEWARE = [
