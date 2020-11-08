c1eddb83f202b158a1ec92c45a4e4e86f8090348
Bruno Cesar <bruunoocesar@gmail.com>
2020-11-08 12:43:27 -0300

    Adiciona edição de campo na listagem do DJango Admin

--- apps/receitas/admin.py
+++ apps/receitas/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from .models import Receita
 
 class IndexReceita(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'categoria', 'tempo_preparo')
+    list_display = ('id', 'nome', 'categoria', 'tempo_preparo', 'publicada', 'pessoa')
     list_display_links = ('id', 'nome')
     search_fields = ('nome',)
     list_filter = ('categoria',)
+    list_editable = ('publicada',)
     list_per_page = 10
 
 admin.site.register(Receita, IndexReceita)
