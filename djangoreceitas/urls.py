348567b18a4e1370b6581b7423416fb9b557b07e
Bruno Cesar <bruunoocesar@gmail.com>
2020-11-08 14:31:29 -0300

    Apresenta imagens de receitas

--- djangoreceitas/urls.py
+++ djangoreceitas/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 print(include('apps.receitas.urls'))
 
 urlpatterns = [
     path('', include('apps.receitas.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
