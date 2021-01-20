c5b160139502536430e2760b569ac7d8f5a4b990
Bruno Cesar <bruunoocesar@gmail.com>
2021-01-20 18:58:30 -0300

    Acessa campos da requisição

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def login(request):
     return render(request, 'usuarios/login.html')
@@ -7,6 +7,15 @@ def logout(request):
     pass
 
 def cadastro(request):
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        password = request.POST['password']
+        password_confirmation = request.POST['password_confirmation']
+        print(nome, email, password, password_confirmation)
+        print(request.POST)
+        return redirect('login')
+    
     return render(request, 'usuarios/cadastro.html')
 
 def dashboard(request):
