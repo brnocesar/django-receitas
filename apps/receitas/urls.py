a50b8e7e4bdcb6abf02c7bff5969e8151f01ff42
Bruno Cesar <bruunoocesar@gmail.com>
2021-01-25 21:24:50 -0300

    Finaliza CRUD implementando edit de receita

--- apps/receitas/urls.py
+++ apps/receitas/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('receitas/<int:receita_id>', views.receita, name='receita'), # mudar o nome dessa rota para receita.show
     path('receitas/criar', views.create, name='receita.create'),
+    path('receitas/editar/<int:receita_id>', views.edit, name='receita.edit'),
     path('receitas/deletar/<int:receita_id>', views.destroy, name='receita.destroy'),
 ]
--- apps/receitas/views.py
+++ apps/receitas/views.py
@@ -49,6 +49,22 @@ def create(request):
     
     return render(request, 'receitas/create.html')
 
+def edit(request, receita_id):
+    if request.method == 'POST':
+        receita = Receita.objects.get(pk=receita_id)
+        receita.nome          = request.POST['nome']
+        receita.ingredientes  = request.POST['ingredientes']
+        receita.modo_preparo  = request.POST['modo_preparo']
+        receita.tempo_preparo = request.POST['tempo_preparo']
+        receita.rendimento    = request.POST['rendimento']
+        receita.categoria     = request.POST['categoria']
+        receita.foto          = request.FILES['foto'] if 'foto' in request.FILES else receita.foto
+        receita.save()
+        
+        return redirect('dashboard') # redirecionar para show
+    
+    return render(request, 'receitas/edit.html', {'receita': get_object_or_404(Receita, pk=receita_id)})
+
 def destroy(request, receita_id):
     # abstrair isso para um metodo que da um retorno mais adequado no caso de nao existir
     # ou usar algo que de pra verificar se nao existe
